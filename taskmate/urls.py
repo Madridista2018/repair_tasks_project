4fe139af8f8acb45e347eab21b53f61204e3a48e
lzh <584909249@qq.com>
2020-10-21 16:05:21 +0800

    'update'

--- taskmate/urls.py
+++ taskmate/urls.py
@@ -4,7 +4,7 @@ from todolist_app import views as todolist_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todolist/',include('todolist_app.urls')),#后面接todolist_app的url
+    path('',include('todolist_app.urls')),#后面接todolist_app的url
     path('account/',include('users_app.urls')),
     path('contact',todolist_view.contact, name = 'contact'),
     path('about-us',todolist_view.about, name = 'about'), #name= 后接view里被分发的函数,第一个是url路径
